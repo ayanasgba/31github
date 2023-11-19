49f2679f227eccd8a8704c423d17e84133092aed
Aiana Sagynbek <ayana12q34@gmail.com>
2023-11-19 14:06:22 +0600

    Sum of multiples

---
+++ Leetcode/SumOfMultiples.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+public class SumOfMultiples {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i=0; i<=n; i++){
+            if(i%3 == 0 || i%5 == 0 || i%7 == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        SumOfMultiples sumOfMultiples = new SumOfMultiples();
+        int res = sumOfMultiples.sumOfMultiples(7);
+        System.out.println("Sum of multiples: " + res);
+    }
+}
