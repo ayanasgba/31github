ed0d9afc8002dd3abe6d6f765d18257d66e1fd98
Aiana Sagynbek <77941434+ayanasgba@users.noreply.github.com>
2023-11-26 20:48:22 +0600

    Big Integer

---
+++ hackerrank/BigInteger.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); 
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        System.out.println(A.add(B));
+        System.out.println(A.multiply(B));
+    }
+}
