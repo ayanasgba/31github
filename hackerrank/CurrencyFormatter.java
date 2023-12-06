99cb4ef7766e075e2dd3e7694f8015ca510f5592
Aiana Sagynbek <77941434+ayanasgba@users.noreply.github.com>
2023-12-06 23:20:15 +0600

    CurrencyFormatter

---
+++ hackerrank/CurrencyFormatter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        
+        Locale indiaLocale = new Locale("en", "IN");
+   
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        // String usFormatted = us.format(payment);
+        // String indiaFormatted = india.format(payment);
+        // String chinaFormatted = china.format(payment);
+        // String franceFormatted = france.format(payment);
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
