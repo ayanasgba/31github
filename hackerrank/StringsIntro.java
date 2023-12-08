c4688b4ecfdb504f1dad806936ba573c4dc3c1d3
Aiana Sagynbek <77941434+ayanasgba@users.noreply.github.com>
2023-12-08 15:42:08 +0600

    StringsIntro

---
+++ hackerrank/StringsIntro.java
@@ -0,0 +1,12 @@
+Scanner sc=new Scanner(System.in);
+String A=sc.next();
+String B=sc.next();
+
+System.out.println(A.length()+B.length());
+System.out.println(A.compareTo(B)>0?"Yes":"No");
+        
+System.out.println(A.substring(0, 1).toUpperCase() +A.substring(1) + " " + B.substring(0,1).toUpperCase()+B.substring(1));
+
+
+
+
